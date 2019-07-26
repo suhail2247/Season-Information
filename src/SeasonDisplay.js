36f5f5f2e1751ce76cff390bca382660ac02636d
suhail2247 <suhail.chouhan786@gmail.com>
2019-07-26 18:26:40 +0530

    Styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+const Season=(latitude,month)=>{
+    if(month>2 && month<9){
+        if(lat>0){
+            //notheren hamisphere
+            return "Summer"
+        }
+        else{
+            //southern hemisphere
+            return "Winter"
+        }
+    }
+    else{
+        if(lat>0){
+            //notheren hamisphere
+            return "Winter"
+        }
+        else{
+            //southern hemisphere
+            return "Summer"
+        }
+    }
+}
 const DisplaySeason=(props)=>{
-    return <div>{props.latitude}</div>
+    const seson=Season(props.latitude,new Date().getMonth());
+    const text = season === "Winter"?"It's To Cold":"It's To Hot";
+    return <div>{text}</div>
 }
 
 export default DisplaySeason;
